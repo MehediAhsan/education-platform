261175aa5ea384cd6574fc9449064f3dd47be88e
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-08-21 22:28:03 +0600

    updated carousel

--- app/page.jsx
+++ app/page.jsx
@@ -156,11 +156,12 @@ const HomePage = () => {
             dynamicHeight={false}
             className="relative"
           >
-            <div className="bg-gray-100 p-6 rounded-lg text-center flex justify-center item-center">
-              <p className="text-gray-700 text-lg md:text-xl w-96">
+            <div className="bg-gray-100 p-6 rounded-lg text-center ">
+              <p className="text-gray-700 text-lg md:text-xl ">
                 "EduConnect has transformed the way I learn. The courses are comprehensive, and the instructors are amazing!"
               </p>
-              <div className="mt-6 w-28 h-28 ">
+              <div className='flex justify-center items-center'>
+              <div className="mt-6 w-20 h-20">
                 <img
                   src="https://images.unsplash.com/photo-1566753323558-f4e0952af115?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=721&q=80"
                   alt="Student"
@@ -171,21 +172,24 @@ const HomePage = () => {
                 </p>
                 <p className="text-gray-500">Business Analyst</p>
               </div>
+              </div>
             </div>
             <div className="bg-gray-100 p-6 rounded-lg text-center">
               <p className="text-gray-700 text-lg md:text-xl">
                 "I've taken several courses on EduConnect, and each one has been a valuable learning experience. Highly recommended!"
               </p>
-              <div className="mt-6">
+              <div className='flex justify-center items-certer'>
+              <div className="mt-6 w-20 h-20">
                 <img
                   src="https://images.unsplash.com/photo-1577880216142-8549e9488dad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
                   alt="Student"
-                  className="w-16 h-16 mx-auto rounded-full"
+                  className=" mx-auto rounded-full"
                 />
                 <p className="text-gray-600 font-semibold text-lg mt-2">
                   John Smith
                 </p>
                 <p className="text-gray-500">Software Engineer</p>
+              </div>
               </div>
             </div>
           </Carousel>
