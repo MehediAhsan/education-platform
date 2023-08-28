721dfe6e0f8dd7d8b3d1270134ff10bd67dd4fc7
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-08-28 23:41:30 +0600

    updated banner

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   const bannerSlides = [
     {
       image: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
-      title: 'Unlock Your Potential',
+      title: 'Find your best course',
       description: 'Elevate your skills with our online courses.',
     },
     {
@@ -138,7 +138,7 @@ const HomePage = () => {
   </div>
 </section>
 
-<div className="container mx-auto py-12">
+    <div className="container mx-auto py-12">
         <section className="bg-white shadow-md rounded-lg p-8">
           <h2 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-8 text-gray-800 text-center">
             What Our Students Say
@@ -157,7 +157,7 @@ const HomePage = () => {
             className="relative"
           >
             <div className="bg-gray-100 p-6 rounded-lg text-center ">
-              <p className="text-gray-700 text-lg md:text-xl ">
+              <p className="text-gray-700 text-lg md:text-xl">
                 "EduConnect has transformed the way I learn. The courses are comprehensive, and the instructors are amazing!"
               </p>
               <div className='flex justify-center items-center'>
