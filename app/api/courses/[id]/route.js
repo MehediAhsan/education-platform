bd1357c41e600d95562cc16fabbf404c7ba3a8c0
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-08-31 22:40:54 +0600

    set router with parameter

---
+++ app/api/courses/[id]/route.js
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server'
+ 
+export async function GET(request, {params}) {
+    const id = params.id;
+    const courses = require('../data.json'); // Adjust the path as needed
+    const course = courses.find(course => course.id === id);
+  
+    return NextResponse.json({ course })
+}
