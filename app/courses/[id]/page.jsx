9f5afb8fab168139fcfe43a52b123b38007d6371
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-09-02 14:22:59 +0600

    setting id params

--- app/courses/[id]/page.jsx
+++ app/courses/[id]/page.jsx
@@ -1,20 +1,19 @@
-'use client'
 import React from 'react'
- 
-import { useRouter } from 'next/navigation'
-// async function fetchCourse(){
-//     const response = await fetch(`http://localhost:3000/api/courses/${id}`)
-//     const course = await response.json();
-//     return course;
-// }
 
-const CoursePage =  () => {
-    // const course = await fetchCourse();
-    const router = useRouter()
- 
+const CoursePage =  async({params: {id}}) => {
+    
+  async function fetchCourse(){
+    const response = await fetch(`http://localhost:3000/api/courses/${id}`)
+    const course = await response.json();
+    return course;
+    }
+  const {course} = await fetchCourse();
+  const {title} = course;
+    
   return (
-    <div>CoursePage{router.query.id}</div>
+    <div>CoursePage{title}</div>
   )
 }
+
 
 export default CoursePage
