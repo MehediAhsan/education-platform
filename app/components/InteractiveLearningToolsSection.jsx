569561e7cd7ea093742c2aae4d3cff205d5f93c8
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-09-07 21:59:12 +0600

    updated section

--- app/components/InteractiveLearningToolsSection.jsx
+++ app/components/InteractiveLearningToolsSection.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { FaGamepad, FaFlask, FaRobot } from 'react-icons/fa';
 
 const InteractiveLearningToolsSection = () => {
   return (
-    <section className="bg-gray-100 py-12">
+    <section className="py-12">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-semibold text-gray-800 mb-8">
+        <h2 className="text-3xl font-semibold mb-14">
           Interactive Learning Tools
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {/* Tool 1: Gamified Quizzes */}
           <div className="bg-white rounded-lg shadow-md p-6">
+            <FaGamepad className="text-4xl text-blue-500 mb-4" />
             <h3 className="text-xl font-semibold text-gray-800 mb-4">
               Gamified Quizzes
             </h3>
@@ -20,6 +22,7 @@ const InteractiveLearningToolsSection = () => {
 
           {/* Tool 2: Virtual Labs */}
           <div className="bg-white rounded-lg shadow-md p-6">
+            <FaFlask className="text-4xl text-blue-500 mb-4" />
             <h3 className="text-xl font-semibold text-gray-800 mb-4">
               Virtual Labs
             </h3>
@@ -30,6 +33,7 @@ const InteractiveLearningToolsSection = () => {
 
           {/* Tool 3: AI-powered Tutors */}
           <div className="bg-white rounded-lg shadow-md p-6">
+            <FaRobot className="text-4xl text-blue-500 mb-4" />
             <h3 className="text-xl font-semibold text-gray-800 mb-4">
               AI-powered Tutors
             </h3>
