0c4e0830e00327fcab7c06d300ca8177fb6500a0
Kazi Tanvirul Isam Sakib <e193003@ugrad.iiuc.ac.bd>
2023-10-14 19:15:06 +0000

    await

--- app/api/courses/[id]/route.js
+++ app/api/courses/[id]/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 export async function GET(request, {params}) {
     const id = params.id;
     const courses = require('../data.json'); // Adjust the path as needed
-    const course = courses.find(course => course.id === id);
+    const course = await courses.find(course => course.id === id);
   console.log(course)
     return NextResponse.json(course)
 }
