25b9b8d53cef223606526d2a95f1cf955a42fae2
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-14 22:01:41 +0600

    update url

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -4,7 +4,7 @@
 import Link from "next/link";
 
 async function fetchCourses(){
-  const response = await fetch("http://127.0.0.1:3000/api/courses")
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/courses`)
   const courses = await response.json();
   return courses;
 }
