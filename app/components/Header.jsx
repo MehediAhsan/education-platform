66043121a3b02f7bd0029eff34061e1c67df56c7
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-14 17:39:06 +0600

    updated navbar

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,60 +1,60 @@
 'use client';
 import Link from "next/link";
 import { useState } from "react";
+import { FaHome, FaEnvelope, FaBook, FaTimes, FaBars, FaBookOpen } from "react-icons/fa";
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const Menus = [
+        {
+            name: 'Home', 
+            link:'/',
+            icon: <FaHome></FaHome>
+        },
+        {
+            name: 'CONTACT US', 
+            link:'/contact',
+            icon: <FaEnvelope></FaEnvelope>
+        },
+        {
+            name: 'Courses', 
+            link:'/courses',
+            icon: <FaBook></FaBook>
+        },
+        {
+            name: 'About', 
+            link:'/about',
+            icon: <></>
+        },
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+    ]
+
+    let [open, setOpen] = useState(false)
 
   return (
-    <nav className="bg-gray-800 py-4 shadow-lg text-white">
-      <div className="container mx-auto px-6">
-        <div className="flex items-center justify-between">
-          <Link href="/" passHref legacyBehavior>
-            <a className="text-2xl font-semibold text-white">
-              EduConnect
-            </a>
-          </Link>
-          <div className="md:hidden">
-            <button
-              className="text-white focus:outline-none"
-              onClick={toggleMenu}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div className={`md:flex ${isMenuOpen ? "block" : "hidden"} mt-4 md:mt-0`}>
-            <NavLink href="/" title="Home" />
-            <NavLink href="/courses" title="Courses" />
-            <NavLink href="/about" title="About Us" />
-            <NavLink href="/contact" title="Contact Us" />
-          </div>
+    <div className="bg-black text-white w-full fixed top-0 left-0 z-50 opacity-80 shadow-xl">
+            <div className="flex items-center justify-between px-14 py-7">
+                <div className="flex justify-center items-center text-2xl font-semibold gap-3">
+                    <FaBookOpen className="text-teal-500 text-3xl"></FaBookOpen>
+                    <Link href="/" legacyBehavior><h1 className="font-semibold font-sans italic cursor-pointer">EduConnect</h1></Link>
+                </div>
+                <div>
+                    <div onClick={() => setOpen(!open)} className="md:hidden absolute top-8 right-10 text-xl">
+                        {
+                            open ? <FaTimes></FaTimes> : <FaBars></FaBars> 
+                        }
+                    </div>
+                    <ul className={`md:flex md:items-center justify-center absolute md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto pl-9 md:pl-0 py-4 md:py-0 uppercase ${open ? 'top-20 opacity-100': 'top-[-490px] md:opacity-100 opacity-0'} `}>
+                        {
+                            Menus.map(Menu => <li key={Menu.name} className="flex justify-center items-center gap-1 md:ml-6 my-6 md:my-0 transition duration-300 hover:text-teal-500 font-medium">
+                                <span className="text-xl text-teal-500">{Menu.icon}</span>
+                               <Link href={Menu.link} legacyBehavior>{Menu.name}</Link> 
+                            </li>)
+                        }
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-    </nav>
   );
 };
-
-const NavLink = ({ href, title }) => (
-  <Link href={href} passHref legacyBehavior>
-    <a className="text-gray-100 hover:text-white block mt-4 md:inline-block md:mt-0 md:ml-4">{title}</a>
-  </Link>
-);
 
 export default Header;
