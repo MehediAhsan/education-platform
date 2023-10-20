6ac7c347c8f8b15c42aaab7e2d633e29b3f00865
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-20 16:00:13 +0600

    contact page create

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,10 +1,26 @@
 
 const ContactPage = () => {
   return (
-    <div className="container w-11/12 mx-auto py-12 pt-28">
-        <h1 className="text-3xl md:text-4xl lg:text-4xl font-semibold mb-8 text-center">
-          Contact Us
-        </h1>
+    <div className="relative">
+      <div className="relative">
+      <img src="https://images.unsplash.com/photo-1587560699334-bea93391dcef?auto=format&fit=crop&q=80&w=1470&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="" />
+      <div className="bg-black absolute inset-0 opacity-70 "></div>
+      </div>
+        <div className="absolute top-44"> 
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10 mb-10 md:mx-28">
+          <div className="w-full">
+            <h1 className="bg-teal-500 text-center uppercase p-2">Address</h1>
+            <p className="bg-gray-900 text-center uppercase p-10 w-11/12 mx-auto -skew-x-3">Dhaka, Banasree</p>
+          </div>
+          <div className="w-full">
+            <h1 className="bg-teal-500 text-center uppercase p-2">Email</h1>
+            <p className="bg-gray-900 text-center uppercase p-10 w-11/12 mx-auto -skew-x-3">xyx@gmail.com</p>
+          </div>
+          <div className="w-full">
+            <h1 className="bg-teal-500 text-center uppercase p-2">Phone</h1>
+            <p className="bg-gray-900 text-center uppercase p-10 w-11/12 mx-auto -skew-x-3">1234 5878 4544</p>
+          </div>
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center justify-center">
           <div>
             <img
@@ -59,6 +75,7 @@ const ContactPage = () => {
               </button>
             </form>
           </div>
+        </div>
         </div>
       </div>
   )
