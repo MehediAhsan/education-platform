81ab7315839d305627ede76a36c9bb5229f43e2f
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-25 21:51:43 +0600

    update login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,3 +1,5 @@
+import { FaGoogle } from "react-icons/fa"
+import Link from "next/link"
 
 const LoginPage = () => {
   return (
@@ -7,19 +9,20 @@ const LoginPage = () => {
       <div className="bg-black absolute inset-0 opacity-80"></div>
       </div>
 
-      <div className="absolute inset-0 top-44 w-4/12 mx-auto shadow-md shadow-teal-500 rounded">
+      <div className="absolute inset-0 top-44 w-4/12 mx-auto shadow-md shadow-teal-200 rounded">
       <form action="" className="w-9/12 mx-auto p-8 flex flex-col gap-6">
             <div>
                 <legend className="text-xl mb-4">Email :</legend>
-                <input type="email" className=" bg-transparent p-2 shadow-sm shadow-white" />
+                <input type="email" className="ml-6 bg-transparent p-2 shadow-sm shadow-white rounded" />
             </div>
             <div>
                 <legend className="text-xl mb-4">Password :</legend>
-                <input type="password" className=" bg-transparent p-2 shadow-sm shadow-white" />
+                <input type="password" className="ml-6 bg-transparent p-2 shadow-sm shadow-white rounded" />
             </div>
             <button className="bg-teal-500 hover:bg-teal-600 w-20 p-2 rounded text-lg">Login</button>
             <p className="text-center text-teal-500 text-2xl">----Or----</p>
-            <button className="border-b-2 rounded border-teal-500 transition duration-300 hover:bg-teal-600 py-2 text-lg shadow-sm shadow-teal-500">Login With Google</button>
+            <button className="flex justify-center items-center gap-2 border-b-2 rounded border-teal-500 transition duration-300 hover:bg-teal-600 py-2 text-lg shadow-sm shadow-teal-500"><span>Login With Google</span> <FaGoogle/></button>
+            <p className="text-center pb-3">Are you new? Please <Link href="/" className="text-teal-400">sign up</Link></p>
         </form>
       </div>
     </div>
